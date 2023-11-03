ba16a21300f845976b7eeed8ac16865f6a093a03
raghavendrasangam <raghavendrasangam0007@gmail.com>
2023-11-03 23:48:06 +0530

    toggle fixed

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ const togglebtn = document.getElementById('nav-toggle');
 const navLinks = document.getElementById('nav-links');
 
 togglebtn.addEventListener('click', ()=>{
-    navLinks.classList.toggle('active')
+    navLinks.classList.toggle('active');
+    
 })
